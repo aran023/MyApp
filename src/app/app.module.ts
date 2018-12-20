6d3c0e5b2cabfd90682782d75de272ab6178ee2b
aran023 <46011947+aran023@users.noreply.github.com>
2018-12-20 08:59:57 +0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SocialInfoPage } from '../pages/social-info/social-info';
+import{SMS} from '@ionic-native/sms';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,SocialInfoPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,SocialInfoPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    SMS,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
